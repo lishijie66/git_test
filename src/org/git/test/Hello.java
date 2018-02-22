b5fb92949348bd1f33e255c2486a22db6b190e2b
lishijie <lisjemail@163.com>
2018-02-22 14:09:20 +0800

    一次性修改了两个文件

--- src/org/git/test/Hello.java
+++ src/org/git/test/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("这是我的git测试项目");
 		System.out.println("修改了一行代码");
+		System.out.println("我很喜欢git,");
 	}
 }
--- src/org/git/test/Test.java
+++ src/org/git/test/Test.java
@@ -3,5 +3,6 @@ package org.git.test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("git测试");
+		System.out.println("我很喜欢git,");
 	}
 }
